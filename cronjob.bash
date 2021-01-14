a4a666257889e2d16a9c3eec73a392a9f3fbcfe5
Miguel Rivera Rios <miguel.e.rivera.rios@gmail.com>
2021-01-13 22:31:10 -0500

    Cron Job

--- cronjob.bash
+++ cronjob.bash
@@ -3,5 +3,5 @@ if [ $? -eq 0 ]; then
   echo "Process running."
 else
   echo "Process not running."
-  env NODE_ENV=production /home/d0oezq0leri9/.nvm/versions/node/v10.17.0/bin/node /home/d0oezq0leri9/public_html/src/bin/www
+  env NODE_ENV=production /home/d0oezq0leri9/.nvm/versions/node/v10.15.0/bin/node /home/d0oezq0leri9/public_html/src/bin/www
 fi

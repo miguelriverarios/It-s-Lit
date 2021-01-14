066080cc435675adbff3760f8f6e58aa9d18b160
Miguel Rivera Rios <miguel.e.rivera.rios@gmail.com>
2021-01-13 21:46:28 -0500

    Cron Job

---
+++ cronjob.bash
@@ -0,0 +1,7 @@
+ps cax | grep node > /dev/null
+if [ $? -eq 0 ]; then
+  echo "Process running."
+else
+  echo "Process not running."
+  env NODE_ENV=production /home/d0oezq0leri9/.nvm/versions/node/v10.17.0/bin/node /home/d0oezq0leri9/public_html/src/bin/www
+fi
